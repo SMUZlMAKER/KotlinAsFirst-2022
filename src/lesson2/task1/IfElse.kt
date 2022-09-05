fb564452b37e707525139d9ec1cc7e39fd15bfcc
Томилов Матвей <ameyka45@gmail.com>
2022-09-05 03:09:28 +0300

    Решено: ageDescription timeForHalfWay whichRookThreatens rookOrBishopThreatens triangleKind

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -4,6 +4,7 @@ package lesson2.task1
 
 import lesson1.task1.discriminant
 import kotlin.math.max
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 // Урок 2: ветвления (здесь), логический тип (см. 2.2).
@@ -68,7 +69,17 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+
+fun ageDescription(age: Int): String {
+    if (age % 100 in 5..20) return "$age лет"
+    return when (age % 10) {
+        1 -> "$age год"
+        in 2..4 -> "$age года"
+        in 5..9, 0 -> "$age лет"
+        else -> "$age лет"
+    }
+}
+
 
 /**
  * Простая (2 балла)
@@ -81,7 +92,16 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    var distance: Double = (t1 * v1 + t2 * v2 + t3 * v3) / 2
+    if (distance <= t1 * v1)
+        return distance / v1
+    distance -= t1 * v1
+    if (distance <= t2 * v2)
+        return t1 + distance / v2
+    distance -= t2 * v2
+    return t1 + t2 + distance / v3
+}
 
 /**
  * Простая (2 балла)
@@ -96,7 +116,15 @@ fun whichRookThreatens(
     kingX: Int, kingY: Int,
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
-): Int = TODO()
+): Int {
+    if ((kingX == rookX1 && kingY == rookY2) || (kingX == rookX2 && kingY == rookY1))
+        return 3
+    if (kingX == rookX2 || kingY == rookY2)
+        return 2
+    if (kingX == rookX1 || kingY == rookY1)
+        return 1
+    return 0
+}
 
 /**
  * Простая (2 балла)
@@ -112,7 +140,17 @@ fun rookOrBishopThreatens(
     kingX: Int, kingY: Int,
     rookX: Int, rookY: Int,
     bishopX: Int, bishopY: Int
-): Int = TODO()
+): Int {
+    for (i in -7..7)
+        if ((kingX == rookX || kingY == rookY) && ((kingX == bishopX + i && kingY == bishopY + i) || (kingX == bishopX - i && kingY == bishopY + i) || (kingX == bishopX + i && kingY == bishopY - i)))
+            return 3
+    for (i in -7..7)
+        if ((kingX == bishopX + i && kingY == bishopY + i) || (kingX == bishopX - i && kingY == bishopY + i) || (kingX == bishopX + i && kingY == bishopY - i))
+            return 2
+    if (kingX == rookX || kingY == rookY)
+        return 1
+    return 0
+}
 
 /**
  * Простая (2 балла)
@@ -122,7 +160,29 @@ fun rookOrBishopThreatens(
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    if (a >= b + c || b >= a + c || c >= a + b)
+        return -1
+    if (a > b && a > c) {
+        if (a.pow(2) > b.pow(2) + c.pow(2))
+            return 2
+        if (a.pow(2) == b.pow(2) + c.pow(2))
+            return 1
+        return 0
+    }
+    if (b > a && b > c) {
+        if (b.pow(2) > a.pow(2) + c.pow(2))
+            return 2
+        if (b.pow(2) == a.pow(2) + c.pow(2))
+            return 1
+        return 0
+    }
+    if (c.pow(2) > a.pow(2) + b.pow(2))
+        return 2
+    if (c.pow(2) == a.pow(2) + b.pow(2))
+        return 1
+    return 0
+}
 
 /**
  * Средняя (3 балла)
