52c66e7734844315db777c9d871083c7f6118626
Томилов Матвей <ameyka45@gmail.com>
2022-09-07 00:46:30 +0300

    lesson2.task 2 решены: isNumberHappy queenThreatens daysInMonth

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.pow
 
 /**
  * Пример
@@ -18,7 +19,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = (number / 1000 + number % 1000 / 100) == (number % 100 / 10) + (number % 10)
 
 /**
  * Простая (2 балла)
@@ -27,7 +28,7 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = (x1==x2||y1==y2)||(x1-x2==y1-y2)||(x1-x2==y2-y1)
 
 
 /**
@@ -36,7 +37,22 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return when (month) {
+        4, 6, 9, 11 -> 30
+        2 -> if (year % 100 == 0)
+            if (year % 400 == 0)
+                29
+            else 28
+        else
+            if (year % 4 == 0)
+                29
+            else
+                28
+
+        else -> 31
+    }
+}
 
 /**
  * Простая (2 балла)
